3b94e54e65d985c417538b6b1ca7b9d797d55943
joao vitor <joaovscamargo01@hotmail.com>
2021-01-14 22:52:56 -0300

    mapa

--- src/pages/PetsMaps.tsx
+++ src/pages/PetsMaps.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
+import { MapContainer, TileLayer } from 'react-leaflet';
 
-import mapMarkerImg from '../images/map-marker.svg';
+import 'leaflet/dist/leaflet.css';
+
+import mapMarkerImg from '../images/iconmap.svg';
 
 import '../styles/pages/ongpet-map.css';
 
@@ -23,9 +26,19 @@ function PetsMap() {
         </footer>
       </aside>
 
-      <div>
+      <MapContainer
+        center={[-23.4360882,-45.0805191]}
+        zoom={15}
+        style={{ width: '100%', height:'100%' }}
+       >
+         <TileLayer 
+         url= "https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
+         
+         />
+         
+        </MapContainer>
 
-      </div>
+        
 
       <Link to="" className="create-ong">
         <FiPlus size={32} color="#FFF" />
